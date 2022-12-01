95f150c1ca428085dfb09731fe721ba3189adf41
sai <sai@DESKTOP-J18DL9U>
2022-11-30 21:00:51 -0800

    [Sachin] Good Morning Code

---
+++ src/test/java/Code/Sample1.java
@@ -0,0 +1,12 @@
+package Code;
+
+public class Sample1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Good Morning1");
+		System.out.println("Good Morning2");
+		System.out.println("Good Morning3");
+		System.out.println("Good Morning4");
+	}
+}
